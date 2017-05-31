72970cb581b662c738a71479d6178bca5d1fe65a
nicholaskunes <kunes.nick0@gmail.com>
2017-05-31 15:56:01 -0700

    Update networker.py

--- networker.py
+++ networker.py
@@ -13,26 +13,19 @@ tickCount = 0;
 
 
 def tick():
-    global tickCount
-    
+    global tickCount    
     #ticker = requests.get('https://www.okcoin.com/api/v1/ticker.do?symbol=btc_usd').json()
     #depth = requests.get('https://www.okcoin.com/api/v1/depth.do?symbol=btc_usd&size=60').json()
     #date = datetime.fromtimestamp(int(ticker['date']))
     #price = float(ticker['ticker']['last'])
     #v_bid = sum([bid[1] for bid in depth['bids']])
-    #v_ask = sum([ask[1] for ask in depth['asks']])
-    
+    #v_ask = sum([ask[1] for ask in depth['asks']])    
     asks, bids = btceapi.getDepth(pair)
-
     print(len(asks), len(bids))
-
     ask_prices, ask_volumes = zip(*asks)
     bid_prices, bid_volumes = zip(*bids)
-    
     print(ask_volumes);
-    print(bid_volumes);
-
-    
+    print(bid_volumes);   
     #collection.insert({'date': date, 'price': price, 'v_bid': v_bid, 'v_ask': v_ask})
     tickCount += 1;
     print(tickCount)
