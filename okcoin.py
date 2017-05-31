ee2a7ffc527f7593a07d0634e6940ecebad417ac
nicholaskunes <kunes.nick0@gmail.com>
2017-05-30 18:38:59 -0700

    Update okcoin.py

--- okcoin.py
+++ okcoin.py
@@ -29,7 +29,7 @@ def tick():
 def main():
     """Run tick() at the interval of every ten seconds."""
     scheduler = BlockingScheduler(timezone=utc)
-    scheduler.add_job(tick, 'interval', seconds=10)
+    scheduler.add_job(tick, 'interval', seconds=1)
     try:
         scheduler.start()
     except (KeyboardInterrupt, SystemExit):
