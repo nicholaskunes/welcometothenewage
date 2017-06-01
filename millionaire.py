00a00392d415fae956b49edec6a5ff5fa21e1588
nicholaskunes <kunes.nick0@gmail.com>
2017-06-01 07:09:06 -0700

    Update millionaire.py

--- millionaire.py
+++ millionaire.py
@@ -33,39 +33,39 @@ for doc in collection.find().limit(num_points):
 
 #while True:
 
-    # Use the first time period (prices1) to generate all possible time series of
-    # appropriate length (180, 360, and 720).
-    timeseries180 = generate_timeseries(prices1, 180)
-    timeseries360 = generate_timeseries(prices1, 360)
-    timeseries720 = generate_timeseries(prices1, 720)
+# Use the first time period (prices1) to generate all possible time series of
+# appropriate length (180, 360, and 720).
+timeseries180 = generate_timeseries(prices1, 180)
+timeseries360 = generate_timeseries(prices1, 360)
+timeseries720 = generate_timeseries(prices1, 720)
 
-    # Cluster timeseries180 in 100 clusters using k-means, return the cluster
-    # centers (centers180), and choose the 20 most effective centers (s1).
-    centers180 = find_cluster_centers(timeseries180, 100)
-    s1 = choose_effective_centers(centers180, 20)
+# Cluster timeseries180 in 100 clusters using k-means, return the cluster
+# centers (centers180), and choose the 20 most effective centers (s1).
+centers180 = find_cluster_centers(timeseries180, 100)
+s1 = choose_effective_centers(centers180, 20)
 
-    centers360 = find_cluster_centers(timeseries360, 100)
-    s2 = choose_effective_centers(centers360, 20)
+centers360 = find_cluster_centers(timeseries360, 100)
+s2 = choose_effective_centers(centers360, 20)
 
-    centers720 = find_cluster_centers(timeseries720, 100)
-    s3 = choose_effective_centers(centers720, 20)
+centers720 = find_cluster_centers(timeseries720, 100)
+s3 = choose_effective_centers(centers720, 20)
 
-    # Use the second time period to generate the independent and dependent
-    # variables in the linear regression model:
-    # Δp = w0 + w1 * Δp1 + w2 * Δp2 + w3 * Δp3 + w4 * r.
-    Dpi_r, Dp = linear_regression_vars(prices2, v_bid2, v_ask2, s1, s2, s3)
+# Use the second time period to generate the independent and dependent
+# variables in the linear regression model:
+# Δp = w0 + w1 * Δp1 + w2 * Δp2 + w3 * Δp3 + w4 * r.
+Dpi_r, Dp = linear_regression_vars(prices2, v_bid2, v_ask2, s1, s2, s3)
 
-    # Find the parameter values w (w0, w1, w2, w3, w4).
-    w = find_parameters_w(Dpi_r, Dp)
+# Find the parameter values w (w0, w1, w2, w3, w4).
+w = find_parameters_w(Dpi_r, Dp)
 
-    # Predict average price changes over the third time period.
-    dps = predict_dps(prices3, v_bid3, v_ask3, s1, s2, s3, w)
-    
-    print(dps)
+# Predict average price changes over the third time period.
+dps = predict_dps(prices3, v_bid3, v_ask3, s1, s2, s3, w)
 
-    # What's your 'Fuck You Money' number?
-    bank_balance = evaluate_performance(prices3, dps, t=0.0035, step=1)
+print(dps)
 
-    print(bank_balance)
+# What's your 'Fuck You Money' number?
+bank_balance = evaluate_performance(prices3, dps, t=0.0035, step=1)
 
-    #time.sleep(15)
+print(bank_balance)
+
+#time.sleep(15)
