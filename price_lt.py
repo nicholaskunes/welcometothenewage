b2a80b15a63f8b034ac45321fe7669834f24bf30
nicholaskunes <kunes.nick0@gmail.com>
2017-06-06 14:26:11 -0700

    Update price_lt.py

--- price_lt.py
+++ price_lt.py
@@ -8,29 +8,29 @@ tickCount = 0;
 def tick():
     global tickCount 
     
-    ticker_ltce = requests.get('https://btc-e.com/api/3/ticker/ltc_usd').json()
-    ticker_btce = requests.get('https://btc-e.com/api/3/ticker/btc_usd').json()
-    ticker_ethe = requests.get('https://btc-e.com/api/3/ticker/eth_usd').json()
+    ticker_ltce = requests.get('https://btc-e.com/api/3/ticker/ltc_usd')
+    ticker_btce = requests.get('https://btc-e.com/api/3/ticker/btc_usd')
+    ticker_ethe = requests.get('https://btc-e.com/api/3/ticker/eth_usd')
     
-    ticker_ltcfinex = requests.get('https://api.bitfinex.com/v1/pubticker/ltcusd').json()
-    ticker_btcfinex = requests.get('https://api.bitfinex.com/v1/pubticker/btcusd').json()
-    ticker_ethfinex = requests.get('https://api.bitfinex.com/v1/pubticker/ethusd').json()
+    ticker_ltcfinex = requests.get('https://api.bitfinex.com/v1/pubticker/ltcusd')
+    ticker_btcfinex = requests.get('https://api.bitfinex.com/v1/pubticker/btcusd')
+    ticker_ethfinex = requests.get('https://api.bitfinex.com/v1/pubticker/ethusd')
     
-    ticker_ltcdax = requests.get('https://api.gdax.com/products/LTC-USD/ticker').json()
-    ticker_btcdax = requests.get('https://api.gdax.com/products/BTC-USD/ticker').json()
-    ticker_ethdax = requests.get('https://api.gdax.com/products/ETH-USD/ticker').json()
+    ticker_ltcdax = requests.get('https://api.gdax.com/products/LTC-USD/ticker')
+    ticker_btcdax = requests.get('https://api.gdax.com/products/BTC-USD/ticker')
+    ticker_ethdax = requests.get('https://api.gdax.com/products/ETH-USD/ticker')
     
-    price_ltce = float(ticker_ltce['ltc_usd']['last'])
-    price_btce = float(ticker_btce['btc_usd']['last'])
-    price_ethe = float(ticker_ethe['eth_usd']['last'])
+    price_ltce = float(ticker_ltce.json()['ltc_usd']['last'])
+    price_btce = float(ticker_btce.json()['btc_usd']['last'])
+    price_ethe = float(ticker_ethe.json()['eth_usd']['last'])
     
-    price_ltcfinex = float(ticker_ltcfinex['last_price'])
-    price_btcfinex = float(ticker_btcfinex['last_price'])
-    price_ethfinex = float(ticker_ethfinex['last_price'])
+    price_ltcfinex = float(ticker_ltcfinex.json()['last_price'])
+    price_btcfinex = float(ticker_btcfinex.json()['last_price'])
+    price_ethfinex = float(ticker_ethfinex.json()['last_price'])
     
-    price_ltcdax = float(ticker_ltcdax['price'])
-    price_btcdax = float(ticker_btcdax['price'])
-    price_ethdax = float(ticker_ethdax['price'])
+    price_ltcdax = float(ticker_ltcdax.json()['price'])
+    price_btcdax = float(ticker_btcdax.json()['price'])
+    price_ethdax = float(ticker_ethdax.json()['price'])
     
     tickCount += 1;
     print("ltce {} btce {} ethe {} [req {}ms]\nltcfinex {} btcfinex {} ethfinex {}[req {}ms]\nltcdax {} btcdax {} ethdax {}[req {}ms]\n".format(price_ltce, price_btce, price_ethe, ticker_ethe.elapsed.total_seconds(), price_ltcfinex, price_btcfinex, price_ethfinex, ticker_ethfinex.elapsed.total_seconds(), price_ltcdax, price_btcdax, price_ethdax, ticker_ethdax.elapsed.total_seconds()))
