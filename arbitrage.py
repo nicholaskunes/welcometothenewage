db95906f1050aced5306534ef750767712ee65ca
Nicholas Kunes <kunes.nick0@gmail.com>
2017-06-17 14:17:49 -0700

    1

--- arbitrage.py
+++ arbitrage.py
@@ -13,23 +13,23 @@ def tick():
 	#usd > alt > btc > usd
 	
 	#BITFINEX PUBLIC TICKER ALT/USD
-	ticker_btcusd = requests.get('https://api.bitfinex.com/v1/pubticker/btcusd');
-	ticker_zecusd = requests.get('https://api.bitfinex.com/v1/pubticker/zecusd');
-	ticker_xmrusd = requests.get('https://api.bitfinex.com/v1/pubticker/xmrusd');
-	ticker_xrpusd = requests.get('https://api.bitfinex.com/v1/pubticker/xrpusd');
-	ticker_dshusd = requests.get('https://api.bitfinex.com/v1/pubticker/dshusd');
+	ticker_btcusd = requests.get('https://api.bitfinex.com/v1/pubticker/btcusd')
+	ticker_zecusd = requests.get('https://api.bitfinex.com/v1/pubticker/zecusd')
+	ticker_xmrusd = requests.get('https://api.bitfinex.com/v1/pubticker/xmrusd')
+	ticker_xrpusd = requests.get('https://api.bitfinex.com/v1/pubticker/xrpusd')
+	ticker_dshusd = requests.get('https://api.bitfinex.com/v1/pubticker/dshusd')
 	
 	#BITFINEX PUBLIC TICKER ALT/BTC
-	ticker_zecbtc = requests.get('https://api.bitfinex.com/v1/pubticker/zecbtc');
-	ticker_xmrbtc = requests.get('https://api.bitfinex.com/v1/pubticker/xmrbtc');
-	ticker_xrpbtc = requests.get('https://api.bitfinex.com/v1/pubticker/xrpbtc');
-	ticker_dshbtc = requests.get('https://api.bitfinex.com/v1/pubticker/dshbtc');
+	ticker_zecbtc = requests.get('https://api.bitfinex.com/v1/pubticker/zecbtc')
+	ticker_xmrbtc = requests.get('https://api.bitfinex.com/v1/pubticker/xmrbtc')
+	ticker_xrpbtc = requests.get('https://api.bitfinex.com/v1/pubticker/xrpbtc')
+	ticker_dshbtc = requests.get('https://api.bitfinex.com/v1/pubticker/dshbtc')
 	
 	#trigger theory:
 	#threshold = (btc_price - ((1 / alt_btc_price) * alt_price)) - ((btc_price * 0.002) * 3);
 	
 	#BITFINEX TRIGGERS
-    threshold_zec = (float(ticker_btcusd.json()['bid']) - ((1 / float(ticker_zecbtc.json()['bid'])) * float(ticker_zecusd.json()['bid']))) - ((float(ticker_btcusd.json()['bid']) * 0.002) * 3);
+    threshold_zec = (float(ticker_btcusd.json()['bid']) - ((1 / float(ticker_zecbtc.json()['bid'])) * float(ticker_zecusd.json()['bid']))) - ((float(ticker_btcusd.json()['bid']) * 0.002) * 3)
 
     
     #if price_btc.min() == float(ticker_btce.json()['btc_usd']['buy']):
@@ -38,7 +38,7 @@ def tick():
 	
 	print("zec threshold is {}".format(threshold_zec))
     
-    tickCount += 1;
+    tickCount += 1
 
 
 def main():
