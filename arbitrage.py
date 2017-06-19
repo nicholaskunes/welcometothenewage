e26b627cc358ca5bf7d3c2850661b0f68b663351
Nicholas Kunes <kunes.nick0@gmail.com>
2017-06-18 23:59:38 -0700

    test

--- arbitrage.py
+++ arbitrage.py
@@ -247,33 +247,55 @@ def tick():
     #trigger theory:
     #threshold = (btc_price - ((1 / alt_btc_price) * alt_price)) - ((btc_price * 0.002) * 3);
     #($bitfinex_btc_ask-((1/$bitfinex_eth_crypto_ask)*$bitfinex_eth_bid))-(($bitfinex_btc_ask*0.002)*3);
+        
+    zec1 = float(ticker_btcusd.json()['ask']) - (float(ticker_btcusd.json()['ask']) * 0.002)
+    zec2 = zec1 / float(ticker_zecbtc.json()['ask'])
+    zec3 = zec2 - (zec2 * 0.002)
+    zec4 = zec3 * float(ticker_zecusd.json()['ask'])
+    zec5 = zec4 - (zec4 * 0.002)
+    threshold_zec = zec5 - float(ticker_btcusd.json()['ask'])
     
-    #BITFINEX TRIGGERS
-    threshold_zec = (float(ticker_btcusd.json()['ask']) - ((1 / float(ticker_zecbtc.json()['ask'])) * float(ticker_zecusd.json()['bid']))) - ((float(ticker_btcusd.json()['ask']) * 0.002) * 3)
-    threshold_xmr = (float(ticker_btcusd.json()['ask']) - ((1 / float(ticker_xmrbtc.json()['ask'])) * float(ticker_xmrusd.json()['bid']))) - ((float(ticker_btcusd.json()['ask']) * 0.002) * 3)
-    threshold_xrp = (float(ticker_btcusd.json()['ask']) - ((1 / float(ticker_xrpbtc.json()['ask'])) * float(ticker_xrpusd.json()['bid']))) - ((float(ticker_btcusd.json()['ask']) * 0.002) * 3)
-    threshold_dsh = (float(ticker_btcusd.json()['ask']) - ((1 / float(ticker_dshbtc.json()['ask'])) * float(ticker_dshusd.json()['bid']))) - ((float(ticker_btcusd.json()['ask']) * 0.002) * 3)
+    xmr1 = float(ticker_btcusd.json()['ask']) - (float(ticker_btcusd.json()['ask']) * 0.002)
+    xmr2 = xmr1 / float(ticker_xmrbtc.json()['ask'])
+    xmr3 = xmr2 - (xmr2 * 0.002)
+    xmr4 = xmr3 * float(ticker_xmrusd.json()['ask'])
+    xmr5 = xmr4 - (xmr4 * 0.002)
+    threshold_xmr = xmr5 - float(ticker_btcusd.json()['ask'])
     
-    threshold = 15
+    xrp1 = float(ticker_btcusd.json()['ask']) - (float(ticker_btcusd.json()['ask']) * 0.002)
+    xrp2 = xrp1 / float(ticker_xrpbtc.json()['ask'])
+    xrp3 = xrp2 - (xrp2 * 0.002)
+    xrp4 = xrp3 * float(ticker_xrpusd.json()['ask'])
+    xrp5 = xrp4 - (xrp4 * 0.002)
+    threshold_xrp = xrp5 - float(ticker_btcusd.json()['ask'])
+    
+    dsh1 = float(ticker_btcusd.json()['ask']) - (float(ticker_btcusd.json()['ask']) * 0.002)
+    dsh2 = dsh1 / float(ticker_dshbtc.json()['ask'])
+    dsh3 = dsh2 - (dsh2 * 0.002)
+    dsh4 = dsh3 * float(ticker_dshusd.json()['ask'])
+    dsh5 = dsh4 - (dsh4 * 0.002)
+    threshold_dsh = dsh5 - float(ticker_btcusd.json()['ask'])
+    
+    threshold = 200
     
     thresholds = np.array([ threshold_zec, threshold_xmr, threshold_xrp, threshold_dsh ])
             
     if thresholds.max() == threshold_zec and threshold_zec >= threshold and cycling == False:
         altcoin = "zec"
         t = threading.Thread(target=zec_cycle)
-        t.start()    
+        #t.start()    
     elif thresholds.max() == threshold_xmr and threshold_xmr >= threshold and cycling == False:
         altcoin = "xmr"
         t = threading.Thread(target=xmr_cycle)
-        t.start() 
+        #t.start() 
     elif thresholds.max() == threshold_xrp and threshold_xrp >= threshold and cycling == False:
         altcoin = "xrp"      
         t = threading.Thread(target=xrp_cycle)  
-        t.start()         
+        #t.start()         
     elif thresholds.max() == threshold_dsh and threshold_dsh >= threshold and cycling == False:
         altcoin = "dsh"
         t = threading.Thread(target=dsh_cycle)
-        t.start() 
+        #t.start() 
     elif cycling == False:
         altcoin = "null"
     else:
