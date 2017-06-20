be20403c761b5c0f70115b7950a71a5cd41c5d39
Nicholas Kunes <kunes.nick0@gmail.com>
2017-06-19 19:08:02 -0700

    test

--- networker.py
+++ networker.py
@@ -1,23 +1,29 @@
 import requests
 from pytz import utc
+from pytz import timezone
+import pytz
 from datetime import datetime
 from pymongo import MongoClient
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 client = MongoClient()
-database = client['btc-e_db']
+database = client['bitfinex_db']
 collection = database['historical_data']
 tickCount = 0;
 
 
 def tick():
     global tickCount 
-    ticker = requests.get('https://btc-e.com/api/3/ticker/btc_usd').json()
-    depth = requests.get('https://btc-e.com/api/3/depth/btc_usd').json()
-    date = datetime.fromtimestamp(int(ticker['btc_usd']['updated']))
-    price = float(ticker['btc_usd']['last'])
-    v_bid = sum([bid[1] for bid in depth['btc_usd']['bids']])
-    v_ask = sum([ask[1] for ask in depth['btc_usd']['asks']]) 
+    ticker = requests.get('https://api.bitfinex.com/v1/pubticker/btcusd').json()
+    depth = requests.get('https://api.bitfinex.com/v1/book/btcusd').json()
+    
+    date_format='%m/%d/%Y %H:%M:%S %Z'
+    date = datetime.now(tz=pytz.utc)
+    date = date.astimezone(timezone('US/Pacific'))
+    
+    price = float(ticker['ask'])
+    v_bid = sum([bid[1] for bid in depth['bids']])
+    v_ask = sum([ask[1] for ask in depth['asks']]) 
     collection.insert({'date': date, 'price': price, 'v_bid': v_bid, 'v_ask': v_ask})
     tickCount += 1;
     print(tickCount)
