85eb4a478f7799fe2fed2a7dcf690967b9a941af
Nicholas Kunes <kunes.nick0@gmail.com>
2017-06-19 19:15:14 -0700

    teset

--- networker.py
+++ networker.py
@@ -22,8 +22,8 @@ def tick():
     date = date.astimezone(timezone('US/Pacific'))
     
     price = float(ticker['ask'])
-    v_bid = sum([bid[1] for bid in depth['bids']])
-    v_ask = sum([ask[1] for ask in depth['asks']]) 
+    v_bid = sum([bid['amount'] for bid in depth['bids']])
+    v_ask = sum([ask['amount'] for ask in depth['asks']]) 
     collection.insert({'date': date, 'price': price, 'v_bid': v_bid, 'v_ask': v_ask})
     tickCount += 1;
     print(tickCount)
