4d6f78d6694f72a217742052479e6efa5b6b11bc
nicholaskunes <kunes.nick0@gmail.com>
2018-05-21 16:42:32 -0700

    Update networker.py

--- networker.py
+++ networker.py
@@ -13,12 +13,12 @@ tickCount = 0;
 
 def tick():
     global tickCount 
-    ticker = requests.get('https://btc-e.com/api/3/ticker/btc_usd').json()
-    depth = requests.get('https://btc-e.com/api/3/depth/btc_usd').json()
+    ticker = requests.get('https://wex.nz/api/3/ticker/btc_usd').json()
+    depth = requests.get('https://wex.nz/api/3/depth/btc_usd?limit=60').json()
     date = datetime.fromtimestamp(int(ticker['btc_usd']['updated']))
     price = float(ticker['btc_usd']['last'])
     v_bid = sum([bid[1] for bid in depth['btc_usd']['bids']])
-    v_ask = sum([ask[1] for ask in depth['btc_usd']['asks']]) 
+    v_ask = sum([ask[1] for ask in depth['btc_usd']['asks']])
     collection.insert({'date': date, 'price': price, 'v_bid': v_bid, 'v_ask': v_ask})
     tickCount += 1;
     print(tickCount)
