e79ae29b3105ec6849bbdde8abcb1231a30cc323
nicholaskunes <kunes.nick0@gmail.com>
2018-05-21 17:19:31 -0700

    Update networker.py

--- networker.py
+++ networker.py
@@ -15,14 +15,14 @@ def tick():
     global tickCount 
     ticker = requests.get('https://wex.nz/api/3/ticker/btc_usd').json()
     depth = requests.get('https://wex.nz/api/3/depth/btc_usd?limit=60').json()
-    date = datetime.fromtimestamp(int(ticker['btc_usd']['updated']))
-    price = float(ticker['btc_usd']['last'])
-    v_bid = sum([bid[1] for bid in depth['btc_usd']['bids']])
-    v_ask = sum([ask[1] for ask in depth['btc_usd']['asks']])
-    collection.insert({'date': date, 'price': price, 'v_bid': v_bid, 'v_ask': v_ask})
+    #date = datetime.fromtimestamp(int(ticker['btc_usd']['updated']))
+    #price = float(ticker['btc_usd']['last'])
+    #v_bid = sum([bid[1] for bid in depth['btc_usd']['bids']])
+    #v_ask = sum([ask[1] for ask in depth['btc_usd']['asks']])
+    #collection.insert({'date': date, 'price': price, 'v_bid': v_bid, 'v_ask': v_ask})
     tickCount += 1;
-    print(tickCount)
-    print("date: {} price: {} v_bid: {} v_ask: {}".format(date, price, v_bid, v_ask))
+    #print(tickCount)
+    #print("date: {} price: {} v_bid: {} v_ask: {}".format(date, price, v_bid, v_ask))
 
 
 def main():
