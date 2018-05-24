d1eaca3a1bc82aea4df1516d26e15398e1518b02
nicholaskunes <kunes.nick0@gmail.com>
2018-05-23 18:20:33 -0700

    Update networker.py

--- networker.py
+++ networker.py
@@ -15,10 +15,10 @@ logging.basicConfig()
 
 def tick():
     global tickCount 
-    ticker = requests.get('https://wex.nz/api/3/ticker/btc_usd').json()
+    ticker = requests.get('https://api.gdax.com/products/BTC-USD/ticker').json()
     depth = requests.get('https://api.gdax.com/products/BTC-USD/book?level=2').json()
-    date = datetime.fromtimestamp(int(ticker['btc_usd']['updated']))
-    price = float(ticker['btc_usd']['last'])
+    date = datetime.strptime(ticker['time'], "%Y-%m-%dT%H:%M:%S.%fZ")
+    price = float(ticker['price'])
     v_bid = sum([float(bid[1]) for bid in depth['bids']])
     v_ask = sum([float(ask[1]) for ask in depth['asks']])
     collection.insert({'date': date, 'price': price, 'v_bid': v_bid, 'v_ask': v_ask})
