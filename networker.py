cc064737c0f08434201a89e6ca37dbc8044aa611
nicholaskunes <kunes.nick0@gmail.com>
2018-05-24 20:28:22 -0700

    Update networker.py

--- networker.py
+++ networker.py
@@ -20,14 +20,13 @@ def tick():
     depth = requests.get('https://api.gdax.com/products/BTC-USD/book?level=2').json()
     request_time = time.clock() - start
     print(request_time)
-    date = datetime.strptime(ticker['time'], "%Y-%m-%dT%H:%M:%S.%fZ")
+    date = datetime.now()
     price = float(ticker['price'])
     v_bid = sum([float(bid[1]) for bid in depth['bids']])
     v_ask = sum([float(ask[1]) for ask in depth['asks']])
     #collection.insert({'date': date, 'price': price, 'v_bid': v_bid, 'v_ask': v_ask})
     tickCount += 1;
-    print(tickCount)
-    print("date: {} price: {} v_bid: {} v_ask: {}".format(date, price, v_bid, v_ask))
+    print("point: {} date: {} price: {} v_bid: {} v_ask: {}".format(tickCount, date, price, v_bid, v_ask))
 
 
 def main():
