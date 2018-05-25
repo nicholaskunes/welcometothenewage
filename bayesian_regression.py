da03ab00238cb75be896e2519056f7fc234c08ce
nicholaskunes <kunes.nick0@gmail.com>
2018-05-25 16:53:30 -0700

    Update bayesian_regression.py

--- bayesian_regression.py
+++ bayesian_regression.py
@@ -89,18 +89,19 @@ def evaluate_performance(prices, dps, t, step):
     bank_balance = 0
     position = 0
     for i in range(720, len(prices) - 1, step):
+        print(i - 720)
         # long position - BUY
-        if dps[i - 720] > t and position <= 0:
-            position += 1
-            bank_balance -= prices[i]
+        #if dps[i - 720] > t and position <= 0:
+        #    position += 1
+        #    bank_balance -= prices[i]
         # short position - SELL
-        if dps[i - 720] < -t and position >= 0:
-            position -= 1
-            bank_balance += prices[i]
+        #if dps[i - 720] < -t and position >= 0:
+        #    position -= 1
+        #    bank_balance += prices[i]
     # sell what you bought
-    if position == 1:
-        bank_balance += prices[len(prices) - 1]
+    #if position == 1:
+    #    bank_balance += prices[len(prices) - 1]
     # pay back what you borrowed
-    if position == -1:
-        bank_balance -= prices[len(prices) - 1]
+    #if position == -1:
+    #    bank_balance -= prices[len(prices) - 1]
     return bank_balance
